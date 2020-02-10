71d94804e2020347de704c9865c0d20d32573dca
Mark Burns <2317002b@student.gla.ac.uk>
2020-02-10 12:50:32 +0000

    Chapter 6 complere: all passed

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -41,17 +41,27 @@ def populate():
     'Django': 32,
     'Other Frameworks' : 16}
 
+    pageviews = {'Official Python Tutorial': 17,
+            'How to Think like a Computer Scientist':15,
+            'Learn Python in 10 Minutes':12,
+            'Official Django Tutorial':18,
+            'Django Rocks':53,
+            'How to Tango with Django':16,
+            'Bottle':24,
+            'Flask':14,
+            }
+
     for cat, cat_data in cats.items():
         c = add_cat(cat,catviews[cat],catlikes[cat])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'],pageviews[p['title']])
     
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
 
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
     p.views=views
