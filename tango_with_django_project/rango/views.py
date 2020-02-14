e2e9b8d41dbc637a72a5b15fcb52d2b7683cad17
Mark Burns <2317002b@student.gla.ac.uk>
2020-02-14 10:23:46 +0000

    chapter 10: all passed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -21,7 +21,7 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    
+    visitor_cookie_handler(request) 
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request): 
